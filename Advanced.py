442cd651502b9b7a1000425d5e682a9fbeb8368e
Datle2004 <150267562+Datle2004@users.noreply.github.com>
2023-12-04 01:54:28 +0700

    Update Advanced.py

--- Advanced.py
+++ Advanced.py
@@ -10,13 +10,12 @@ class Advanced():
         self.create.rows = 100
         self.create.cols = 100
         self.start = [0,0]
-        self.end = [100,100]
+        self.end = [99,99]
+        self.middle = [0,0]
         self.create.grid = [[0]*100 for i in range(100)]
         self.d = [0,0]
-    def update_start_grid(self,start,end):
-        #direction
-        i = start[0]
-        j = start[1]
+        self.m = [0,0]
+    def update_direction(self,start,end):
         for i in range(0,2):
             if end[i] > start[i]:
                 self.d[i] = 1
@@ -27,15 +26,45 @@ class Advanced():
                     self.d[i] = 1
                 else:
                     self.d[i] = -1
-        self.create.grid[i+self.d[0]][j+self.d[1]] = 1
-        self.create.grid[i][j+self.d[1]] = 1
-        self.create.grid[i+self.d[0]][j] = 1
-        self.create.grid[i-self.d[1]][j+self.d[0]] = 1
-        if self.d[0]*self.d[1] > 0:
-            self.create.grid[i][j-self.d[1]] = 1
+    def update_middle_direction(self):
+        x1,y1 = self.start
+        x2,y2 = self.end
+        if self.d[0]*(x2 - x1) > self.d[1]*(y2 - y1):
+            self.m[0] = self.d[0]
+            self.m[1] = -1 * self.d[1]
+        elif self.d[0]*(x2 - x1) < self.d[1]*(y2 - y1):
+            self.m[0] = -1 * self.d[0]
+            self.m[1] = self.d[1]
         else:
-            self.create.grid[i-self.d[0]][j] = 1
-        print(start)
+            self.m[0] = self.d[0]
+            self.m[1] = self.d[1]
+    def update_start_grid(self,start,d):
+        #direction
+        i,j = start 
+        self.create.grid[i+d[0]][j+d[1]] = 1
+        self.create.grid[i][j+d[1]] = 1
+        self.create.grid[i+d[0]][j] = 1
+        self.create.grid[i-d[1]][j+d[0]] = 1
+        if d[0]*d[1] > 0:
+            self.create.grid[i][j-d[1]] = 1
+        else:
+            self.create.grid[i-d[0]][j] = 1
+    def finish(self,d):
+        i,j = self.end
+        self.create.grid[i+1][j] = 1
+        self.create.grid[i-1][j] = 1
+        self.create.grid[i][j+1] = 1
+        self.create.grid[i][j-1] = 1
+        self.create.grid[i+d[0]][j+d[1]] = 1
+    def find_middle(self):
+        x1,y1 = self.start
+        x2,y2 = self.end
+        D = -1*self.d[0]*self.m[1] + self.d[1]*self.m[0]
+        Dx = -1 *(self.d[0]*x1 - self.d[1]*y1)*self.m[1] + self.d[1]*(self.m[0]*x2-self.m[1]*y2)
+        Dy = self.d[0]*(self.m[0]*x2 - self.m[1]*y2) - self.m[0]*(self.d[0]*x1-self.d[1]*y1)
+        return [Dx//D,Dy//D]
+        
+
     def move_pos(self,text,i):
         return self.create.draw_input(text,i)
     def main(self):
@@ -46,6 +75,7 @@ class Advanced():
         pause = False
         start = False
         check = False
+        check_middle = False
         while run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -73,15 +103,22 @@ class Advanced():
                         pause = not pause
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
-                        self.update_start_grid(self.start,self.end)
+                        self.update_direction(self.start,self.end)
+                        if (sum(self.start) + sum(self.end)) % 2 == 1:
+                            self.end[1] -= self.d[1]
+                        self.update_middle_direction()
+                        if self.m != self.d:
+                            check_middle = True
+                            middle = self.find_middle()
+                        self.update_start_grid(self.start,self.d)
                         start = True
                         pause = False
 
             # Draw the grid and cells
             self.create.draw_grid()
             self.create.draw_cells()
-            pygame.draw.rect(self.screen,"yellow",(self.start[0]*self.create.cell_size +2, self.start[1]*self.create.cell_size + 2,self.create.cell_size - 2,self.create.cell_size - 2))
-            pygame.draw.rect(self.screen,"red",(self.end[0]*self.create.cell_size +2, self.end[1]*self.create.cell_size + 2,self.create.cell_size - 2,self.create.cell_size - 2))
+            pygame.draw.rect(self.screen,"yellow",(self.start[1]*self.create.cell_size +2, self.start[0]*self.create.cell_size + 2,self.create.cell_size - 2,self.create.cell_size - 2))
+            pygame.draw.rect(self.screen,"red",(self.end[1]*self.create.cell_size +2, self.end[0]*self.create.cell_size + 2,self.create.cell_size - 2,self.create.cell_size - 2))
             #draw button
             self.create.draw_pause_button(pause,start)
             self.create.draw_reset_button()
@@ -94,6 +131,17 @@ class Advanced():
                 check = False
             else:
                 check = True
+            if check_middle:
+                if self.create.COUNT ==  abs(middle[0] - self.start[0]) * 4:
+                    self.create.grid = [[0]*100 for i in range(100)]
+                    self.update_start_grid(middle,self.m)
+                if self.create.COUNT == (abs(middle[0] - self.start[0]) + abs(self.end[0] - middle[0])) * 4:
+                    self.create.grid = [[0]*100 for i in range(100)]
+                    self.finish(self.m)
+            else:
+                if self.create.COUNT == abs(self.end[0]-self.start[0])*4:
+                    self.create.grid = [[0]*100 for i in range(100)]
+                    self.finish(self.d)
 
             # Update the display
             pygame.display.update()
